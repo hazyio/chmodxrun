746e2f9a8f227b23b863e83044603ee22250eb65
Daniel Enubiaka <danielenubiakamail@gmail.com>
2026-08-26 09:01:49 +0100

    feat: enhance terminal management by tracking running terminals and ensuring idle terminal usage

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,28 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import { registerCommands } from "./commands";
+export const runningTerminals = new WeakSet<vscode.Terminal>();
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Mark terminal as busy when a command starts.
+  context.subscriptions.push(
+    vscode.window.onDidStartTerminalShellExecution((event) => {
+      runningTerminals.add(event.terminal);
+    }),
+  );
+
+  // Mark terminal as idle when the command ends.
+  context.subscriptions.push(
+    vscode.window.onDidEndTerminalShellExecution((event) => {
+      runningTerminals.delete(event.terminal);
+    }),
+  );
+
+  // Clean up closed terminals.
+  context.subscriptions.push(
+    vscode.window.onDidCloseTerminal((terminal) => {
+      runningTerminals.delete(terminal);
+    }),
+  );
   registerCommands(context);
 }
 
